8876352b1bd9025fc6fc78822b89f3749b485d46
saulotarsobc <saulotarsobc@gmail.com>
2022-11-11 16:34:55 -0300

    :fire:

--- dist/middlewares/validaToken.js
+++ dist/middlewares/validaToken.js
@@ -3,9 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const validaToken = (0, express_1.default)();
 validaToken.use((req, res, next) => {
-    next();
+    if (req.body.token == process.env.TOKEN) {
+        next();
+    }
+    else {
+        res.status(401).json({
+            msg: 'invalid token'
+        });
+    }
 });
 exports.default = validaToken;
--- src/middlewares/validaToken.ts
+++ src/middlewares/validaToken.ts
@@ -1,9 +1,16 @@
+require("dotenv").config();
 import express from 'express';
 
 const validaToken = express();
 
-validaToken.use((req, res, next)=>{
-    next();
-})
+validaToken.use((req, res, next) => {
+    if (req.body.token == process.env.TOKEN) {
+        next();
+    } else {
+        res.status(401).json({
+            msg: 'invalid token'
+        });
+    }
+});
 
 export default validaToken;
